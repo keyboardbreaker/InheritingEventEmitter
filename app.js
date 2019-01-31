5ce6aec7f24a3208909c1f17dcd68df0d4d2bd58
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-31 15:53:54 +0000

    JS can deal with binary data with es6

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var buf = new Buffer.from('hello', 'utf8'); 
-//use buffer.from as buffer is plan deprecated
+//byte = 8bits 01010101
 
-console.log(buf); //hexadecimal
-console.log(buf.toString()); //string equivalent
-console.log(buf.toJSON()); // object
-console.log(buf[2]); //behaves like an array, can retrieve values
+//8 bytes, 8 x8 = 64 bits of zeros and ones
+//from v8 JS Engine
+var buffer = new ArrayBuffer(8); 
+var view = new Int32Array(buffer); // a number, integer, stored with 32 bits
+// can only store 2 numbers in view.
+//as each number is 32 bits
 
-buf.write('wo'); //can write to buffer
-console.log(buf.toString()); //show  results
+view[0] = 10;
+view[1] = 15;
+console.log(view);
+
+//cant add another number, wont error but no storage from view[2];
